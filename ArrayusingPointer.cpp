bfcab54cf340547f2b8fd3de26325086606af839
abhishek1217 <66879777+abhishek1217@users.noreply.github.com>
2020-12-29 17:10:56 +0530

    Add files via upload

---
+++ ArrayusingPointer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[5];
+	int *p = a;
+	cout<<"Enter elements: ";
+	for(int i=0;i<5;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"You entered: ";
+	for(int i=0;i<5;i++)
+	{
+		cout<<*p<<" ";
+		p++;
+	}
+}

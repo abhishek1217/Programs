a5a39b8a22d1887065452262f443cccb59c30d1c
abhishek1217 <66879777+abhishek1217@users.noreply.github.com>
2021-10-19 21:01:06 +0530

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -9,20 +9,6 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-# password = ""
-# for letter in range(0,nr_letters):
-# 	password += random.choice(letters)
-# for symbol in range(0,nr_symbols):
-# 	password += random.choice(symbols)
-# for number in range(0,nr_numbers):
-# 	password += random.choice(numbers)
-# print(password)
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 password = []
 for letter in range(0,nr_letters):
 	password.append(random.choice(letters))
